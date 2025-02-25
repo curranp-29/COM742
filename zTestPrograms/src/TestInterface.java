e6605bd1e3a51489bb178d6de28a628eb676437d
Paul Curran <pcurran84@outlook.com>
2025-02-25 21:57:18 +0000

    //Update

---
+++ zTestPrograms/src/TestInterface.java
@@ -0,0 +1,12 @@
+public class TestInterface implements MyMathInterface{
+    @Override
+    public void squared(int x) {
+        System.out.println("Interface: " + x * x);
+    }
+
+    @Override
+    public int cubed(int x) {
+        System.out.println("Override" + x * x * x);
+        return x * x * x;
+    }
+}
